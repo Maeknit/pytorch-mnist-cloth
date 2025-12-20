bd8c50aca88769f3170f03be2fbbf331b06ddcec
maeknit <dushzhanov_987@mail.ru>
2025-12-20 22:55:44 +0300

    refix test dataset

--- tests/tests_dataset.py
+++ tests/tests_dataset.py
@@ -1,18 +1,49 @@
 from src.dataset import get_loaders
+import torch
+from torch.utils.data import DataLoader
 
-def test_get_loaders():
+def test_get_loaders_basic():
     train_loader, test_loader = get_loaders(batch_size=32)
-    from torch.utils.data import DataLoader
+
+    # Инициализация
     assert isinstance(train_loader, DataLoader)
     assert isinstance(test_loader, DataLoader)
 
-    # берём один batch и проверяем форму
+    # Есть данные?
+    assert len(train_loader) > 0
+    assert len(test_loader) > 0
+
+def test_train_batch_shape_and_types():
+    batch_size = 32
+    train_loader, _ = get_loaders(batch_size=batch_size)
+
     images, labels = next(iter(train_loader))
-    # FashionMNIST: [batch_size, 1, 28, 28]
+
     assert images.ndim == 4
+    assert images.shape[0] == batch_size
     assert images.shape[1] == 1
     assert images.shape[2] == 28
     assert images.shape[3] == 28
 
-    # и что есть столько же меток, сколько картинок
+    assert isinstance(images, torch.Tensor)
+    assert isinstance(labels, torch.Tensor)
+
+    assert images.dtype == torch.float32
+    assert labels.dtype in (torch.int64, torch.long)
+
     assert images.shape[0] == labels.shape[0]
+
+def test_multiple_batches_iteration():
+    batch_size = 16
+    train_loader, _ = get_loaders(batch_size=batch_size)
+
+    # Пройдемся по нескольким батчам, чтобы убедиться, что итерация стабильная
+    num_batches_to_check = 3
+    for i, (images, labels) in enumerate(train_loader):
+        # Проверяем только первые несколько батчей, чтобы не тормозить CI
+        if i >= num_batches_to_check:
+            break
+
+        assert images.ndim == 4
+        assert images.shape[1:] == (1, 28, 28)
+        assert images.shape[0] == labels.shape[0]
